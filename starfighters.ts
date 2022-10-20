1d0551fb9d5d9c4633bfafbf29a7ca71bc43a74f
Gustavo Goulart <66621683+gosttavo@users.noreply.github.com>
2022-10-20 16:13:13 -0300

    Add files via upload

---
+++ starfighters.ts
@@ -0,0 +1,19 @@
+import { Spacecraft, Containership } from './base-ships'
+
+export class MilleniumFalcon extends Spacecraft implements Containership { 
+
+    cargoContainer: number;
+
+    constructor(){
+        super('hyperdrive');
+        this.cargoContainer = 5;
+    }
+
+    jumpIntoHyperspace() {
+        if (Math.random() >= 0.5){
+            super.jumpIntoHyperspace();
+        } else{
+            console.log('Failed');
+        }
+    }
+}
