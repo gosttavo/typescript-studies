b5f19f455cb616c1048de6865eb5b289c7fd0dc6
Gustavo Goulart <66621683+gosttavo@users.noreply.github.com>
2022-10-20 16:14:30 -0300

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,13 @@
-
+"use strict";
+exports.__esModule = true;
+var base_ships_1 = require("./base-ships");
+var starfighters_1 = require("./starfighters");
+var _ = require("lodash");
+//utilizando uma função importada da biblioteca lodash
+console.log(_.pad('Typescript Examples', 40, '='));
+var ship = new base_ships_1.Spacecraft('hyperdrive');
+ship.jumpIntoHyperspace();
+var falcon = new starfighters_1.MilleniumFalcon();
+falcon.jumpIntoHyperspace();
+var goodForTheJob = function (ship) { return ship.cargoContainer > 2; };
+console.log("Is falcon good for the job? ".concat(goodForTheJob(falcon) ? 'yes' : 'no'));
